8e9cad9cdb168af6d241af74116fb5161caf6a83
GUILHERME SILVEIRA VAZ <04626245013@senacrs.edu.br>
2026-05-13 17:11:05 -0300

    feat: mockup telas, falta implementar logica react/js

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Aside from './components/Aside'
+import Progresso from './components/Progresso'
+import TreinoCasa from './components/TreinoCasa'
+import TreinoAcademia from './components/TreinoAcademia'
+import Corrida from './components/Corrida'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    // h-screen = app ocupando toda a tela, overflow hidden = sem scrollbar
+    // flex-1 é para o aside e o progresso ocuparem todo o espaço que sobrar entre o header e o footer
+    <div className='h-screen flex flex-col overflow-hidden'>
     <Navbar/>
+    <div className='flex flex-1 overflow-hidden'> 
     <Aside/>
+    <TreinoCasa/>
+    </div>
     </div>
   )
 }
--- home-page/src/components/Aside.jsx
+++ home-page/src/components/Aside.jsx
@@ -1,13 +1,13 @@
 function Aside(){
     return(
+        // h-full = heigth/screen ocupando tudo
         <div>
-            <aside className="flex h-screen">
-                <div className="flex flex-col bg-linear-to-b from-black to-70% to-red-800 text-white w-60 items-center gap-5 pt-5">    
+            <aside className=" h-full flex flex-col bg-linear-to-b from-black to-70% to-red-800 text-white w-60 items-center gap-5 pt-5">
                 <button className="bg-red-600 rounded-2xl h-[3.75rem] w-[12.625rem] cursor-pointer"><p>Progresso</p></button>
+                <button className="bg-red-600 rounded-2xl h-[3.75rem] w-[12.625rem] cursor-pointer"><p>Cronograma de treinos</p></button>
                 <button className="bg-red-600 rounded-2xl h-[3.75rem] w-[12.625rem] cursor-pointer"><p>Treino em casa</p></button>
                 <button className="bg-red-600 rounded-2xl h-[3.75rem] w-[12.625rem] cursor-pointer"><p>Treino na academia</p></button>
                 <button className="bg-red-600 rounded-2xl h-[3.75rem] w-[12.625rem] cursor-pointer"><p>Corrida</p></button>
-                </div>
             </aside>
         </div>
     )

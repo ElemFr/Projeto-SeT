720451e3600c84b0018cb93272b7849f52cc226f
Guilherme Silveira Vaz <bullybilharva@gmail.com>
2026-05-17 17:48:08 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage'
-import LoginForms from './pages/LoginForms'
+import Login from './pages/Login'
 import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginForms />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   )
